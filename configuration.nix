21f9fd5028bd6deae32cfbfa9a25dd6f49a94308
Zack Glenn <ZackGlenn05@gmail.com>
2024-10-22 14:51:18 -0600

    use flakes

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
@@ -10,7 +10,7 @@
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
       # home manager
-      <home-manager/nixos>
+      inputs.home-manager.nixosModules.default
     ];
 
   # Enable flakes
