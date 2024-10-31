11d330a783c32e7d9f066753646e01b71585b2dd
Zack Glenn <ZackGlenn05@gmail.com>
2024-10-31 13:28:53 -0600

    add mini.nvim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -215,8 +215,29 @@ require("todo-comments").setup({
 	signs = false,
 })
 
+-- Better Around/Inside textobjects
+require("mini.ai").setup({ n_lines = 500 })
+
+-- Add/delete/replace surroundings (brackets, quotes, etc.)
+require("mini.surround").setup()
+
+-- Simple and easy statusline.
+--  You could remove this setup call if you don't like it,
+--  and try some other statusline plugin
+local statusline = require("mini.statusline")
+-- set use_icons to true if you have a Nerd Font
+statusline.setup({ use_icons = vim.g.have_nerd_font })
+
+-- You can configure sections in the statusline by overriding their
+-- default behavior. For example, here we set the section for
+-- cursor location to LINE:COLUMN
+---@diagnostic disable-next-line: duplicate-set-field
+statusline.section_location = function()
+	return "%2l:%-2v"
+end
+
+---@diagnostic disable-next-line: redundant-parameter
 require("nvim-treesitter").setup({
-	-- Autoinstall languages that are not installed
 	auto_install = false,
 	highlight = {
 		enable = true,
