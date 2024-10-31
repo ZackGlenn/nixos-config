c1a2471d5ffa6f71a8894a74a545c1089521a2b3
Zack Glenn <ZackGlenn05@gmail.com>
2024-10-31 13:35:53 -0600

    add wl-clipboard

--- configuration.nix
+++ configuration.nix
@@ -122,6 +122,7 @@
     # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     neovim
     kitty
+    wl-clipboard
   ];
 
   # enable git and lazygit
