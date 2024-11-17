0daa5ca9b6d2c90e63f4f56e26102641e1c4de02
Zack Glenn <ZackGlenn05@gmail.com>
2024-11-16 22:05:23 -0700

    Move nvim config into its own file
    fix paths to subifles

--- home/home.nix
+++ home/home.nix
@@ -1,6 +1,7 @@
   # Home manager config
 {config, lib, pkgs, pkgs-unstable, ... }:
 {
+  imports = [./nvim/nvim.nix];
 
   home.packages = with pkgs; [
     bat
@@ -30,82 +31,6 @@
   };
 
   programs.waybar = import ./waybar/waybar.nix;
-
-  programs.neovim = {
-    enable = true;
-    package = pkgs-unstable.neovim-unwrapped;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-
-    extraLuaConfig = "
-      ${builtins.readFile ./nvim/options.lua}
-      ${builtins.readFile ./nvim/keymaps.lua}
-      ${builtins.readFile ./nvim/gitsigns.lua}
-      ${builtins.readFile ./nvim/which_key.lua}
-      ${builtins.readFile ./nvim/conform.lua}
-      ${builtins.readFile ./nvim/cmp.lua}
-      ${builtins.readFile ./nvim/todo_comments.lua}
-      ${builtins.readFile ./nvim/mini.lua}
-      ${builtins.readFile ./nvim/treesitter.lua}
-      ${builtins.readFile ./nvim/telescope.lua}
-      ${builtins.readFile ./nvim/lsp.lua}
-      ${builtins.readFile ./nvim/toggleterm.lua}
-      ${builtins.readFile ./nvim/aerial.lua}
-      ${builtins.readFile ./nvim/ibl.lua}
-    ";
-
-    plugins = with pkgs.vimPlugins; [
-      vim-sleuth
-      gitsigns-nvim
-      which-key-nvim
-      nvim-lspconfig
-      conform-nvim
-      nvim-cmp
-      tokyonight-nvim
-      todo-comments-nvim
-      plenary-nvim
-      telescope-fzf-native-nvim
-      telescope-ui-select-nvim
-      cmp-nvim-lsp
-      fidget-nvim
-      luasnip
-      cmp_luasnip
-      cmp-nvim-lsp
-      cmp-path
-      toggleterm-nvim
-      aerial-nvim
-      lspkind-nvim
-      indent-blankline-nvim
-
-      pkgs-unstable.vimPlugins.lazydev-nvim
-      pkgs-unstable.vimPlugins.mini-nvim
-
-      (nvim-treesitter.withPlugins (p: [
-        p.nix
-        p.lua
-        p.rust
-        p.python
-        p.julia
-        p.cpp
-        p.bash
-        p.fish
-        p.toml
-        p.yaml
-        p.json
-        p.query
-        p.vimdoc
-      ]))
-    ];
-
-    extraPackages = with pkgs; [
-      lua-language-server
-      nil
-      stylua
-      rust-analyzer
-      pyright
-    ];
-  };
 
   services.dunst.enable = true;
   services.playerctld.enable = true;
---
+++ home/nvim/nvim.nix
@@ -0,0 +1,79 @@
+{pkgs, pkgs-unstable, ...}:
+
+{
+programs.neovim = {
+    enable = true;
+    package = pkgs-unstable.neovim-unwrapped;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    extraLuaConfig = "
+      ${builtins.readFile ./options.lua}
+      ${builtins.readFile ./keymaps.lua}
+      ${builtins.readFile ./gitsigns.lua}
+      ${builtins.readFile ./which_key.lua}
+      ${builtins.readFile ./conform.lua}
+      ${builtins.readFile ./cmp.lua}
+      ${builtins.readFile ./todo_comments.lua}
+      ${builtins.readFile ./mini.lua}
+      ${builtins.readFile ./treesitter.lua}
+      ${builtins.readFile ./telescope.lua}
+      ${builtins.readFile ./lsp.lua}
+      ${builtins.readFile ./toggleterm.lua}
+      ${builtins.readFile ./aerial.lua}
+      ${builtins.readFile ./ibl.lua}
+    ";
+
+    plugins = with pkgs.vimPlugins; [
+      vim-sleuth
+      gitsigns-nvim
+      which-key-nvim
+      nvim-lspconfig
+      conform-nvim
+      nvim-cmp
+      tokyonight-nvim
+      todo-comments-nvim
+      plenary-nvim
+      telescope-fzf-native-nvim
+      telescope-ui-select-nvim
+      cmp-nvim-lsp
+      fidget-nvim
+      luasnip
+      cmp_luasnip
+      cmp-nvim-lsp
+      cmp-path
+      toggleterm-nvim
+      aerial-nvim
+      lspkind-nvim
+      indent-blankline-nvim
+
+      pkgs-unstable.vimPlugins.lazydev-nvim
+      pkgs-unstable.vimPlugins.mini-nvim
+
+      (nvim-treesitter.withPlugins (p: [
+        p.nix
+        p.lua
+        p.rust
+        p.python
+        p.julia
+        p.cpp
+        p.bash
+        p.fish
+        p.toml
+        p.yaml
+        p.json
+        p.query
+        p.vimdoc
+      ]))
+    ];
+
+    extraPackages = with pkgs; [
+      lua-language-server
+      nil
+      stylua
+      rust-analyzer
+      pyright
+    ];
+};
+}
