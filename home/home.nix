80d50e49b4a4a759bdfb017edce46364e1855431
Zack Glenn <ZackGlenn05@gmail.com>
2024-11-21 14:21:07 -0700

    add tofi themeing

--- home/home.nix
+++ home/home.nix
@@ -28,7 +28,19 @@
 
   programs.tofi = {
     enable = true;
-    settings.terminal = "kitty";
+    settings = {
+      terminal = "kitty";
+      width = "100%";
+      height = "100%";
+      border-width = "0";
+      outline-width = "0";
+      padding-left = "35%";
+      padding-top = "35%";
+      result-spacing = "25";
+      num-results = "5";
+      font = "JetBrainsMono";
+      background-color = "#000A";
+    };
   };
 
   programs.btop = {
