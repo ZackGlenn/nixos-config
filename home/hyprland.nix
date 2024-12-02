45b2d71a85f95e5e7119ab83adec4513d618920f
Zack Glenn <ZackGlenn05@gmail.com>
2024-12-01 23:35:02 -0700

    use hyprcursor to set cursor themes for non-gtk applications

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -31,6 +31,7 @@ monitor = ", preferred, auto, auto";
 # or execute your favorite apps at launch like this:
 
 exec-once = [
+  "hyprctl setcursor Bibata-Original-Classic 16"
   "lxqt-policykit-agent"
   "qutebrowser --nowindow" # autostart qb to improve time opening first window
   "$terminal"
