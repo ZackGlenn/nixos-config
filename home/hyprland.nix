9468cde9ab1e7103648b35d05ba4abbd44e959ac
Zack Glenn <ZackGlenn05@gmail.com>
2024-12-01 19:25:12 -0700

    spawn kitty on the correct workspace

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -34,7 +34,7 @@ exec-once = [
   "lxqt-policykit-agent"
   "qutebrowser --nowindow" # autostart qb to improve time opening first window
   "$terminal"
-  "[workspace special silent] $terminal"
+  "[workspace special:magic silent] $terminal"
 ];
 
 
