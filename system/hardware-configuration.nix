b67f19e2e636e39f04c2dab45622741e8513e604
Zack Glenn <ZackGlenn05@gmail.com>
2024-12-21 14:50:55 -0700

    increase minimum gpu frequency

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -35,4 +35,9 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  services.tlp = {
+    enable = true;
+    settings.INTEL_GPU_MIN_FREQ_ON_AC = 500;
+  };
 }
