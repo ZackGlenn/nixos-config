6a350d6898afe4a3dea4d88499abab460159b615
Zack Glenn <ZackGlenn05@gmail.com>
2025-01-19 23:12:25 -0700

    fix nfu alias

--- home/fish.nix
+++ home/fish.nix
@@ -4,7 +4,7 @@ programs.fish = {
   enable = true;
   shellAliases = {
     nrs = "sudo nixos-rebuild switch --flake ~/nixos-config/";
-    nfu = "nix flake update ~/nixos-config";
+    nfu = "nix flake update --flake ~/nixos-config";
   };
   interactiveShellInit = "
     set fish_greeting # Disable greeting
