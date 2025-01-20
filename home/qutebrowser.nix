e67b45549d58accd8461102ae059b95643f54b22
Zack Glenn <ZackGlenn05@gmail.com>
2025-01-19 19:41:39 -0700

    enable qutebrowser widevine support

--- home/qutebrowser.nix
+++ home/qutebrowser.nix
@@ -1,5 +1,8 @@
 {...}:
 {
+nixpkgs.overlays = [
+  (final: prev: {qutebrowser = prev.qutebrowser.override { enableWideVine = true; }; })
+];
 programs.qutebrowser = {
   enable = true;
   keyBindings = {
