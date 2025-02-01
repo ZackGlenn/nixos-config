9bd2316d3902bdfa5ac033440e776eb30a2057cf
Zack Glenn <ZackGlenn05@gmail.com>
2025-01-31 20:40:27 -0700

    add greasemonkey script to automatically set youtube videos to theater mode

--- home/qutebrowser.nix
+++ home/qutebrowser.nix
@@ -1,4 +1,4 @@
-{...}:
+{pkgs, ...}:
 {
 programs.qutebrowser = {
   enable = true;
@@ -62,5 +62,30 @@ programs.qutebrowser = {
     url.default_page = "qute://start";
     url.start_pages = "qute://start";
   };
+  greasemonkey = [
+    (pkgs.writeText "YouTube-Theater-Mode.js" ''
+      // ==UserScript==
+      // @name        YouTube - Always Theater Mode
+      // @namespace   r-a-y/youtube/theater
+      // @description Set the default viewing mode to Theater Mode.
+      // @include     https://www.youtube.com/*
+      // @version     1.4.4
+      // @grant       none
+      // @run-at      document-start
+      // @license     GPL v3
+      // ==/UserScript==
+       
+      // Toggle Theater Mode after YouTube finishes loading a video.
+      window.addEventListener("yt-navigate-finish", function(event) {
+        var newPlayer = document.querySelector('button.ytp-size-button')
+       
+        setTimeout(()=> {
+          if ( newPlayer && null === document.getElementsByTagName('ytd-watch-flexy')[0].getAttribute('full-bleed-player') ) {
+            newPlayer.click()
+          }
+        }, 600)
+      })
+    '')
+  ];
 };
 }
