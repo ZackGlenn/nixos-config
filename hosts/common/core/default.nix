ee0b1ace9e17bc75224fe9458c53081b6c9c9305
Zack Glenn <ZackGlenn05@gmail.com>
2025-04-21 15:02:44 -0600

    add basics to hosts/common/core

---
+++ hosts/common/core/default.nix
@@ -0,0 +1,17 @@
+{inputs, outputs, ...}: {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    ./locale.nix
+    ./nix.nix
+    ./sops.nix
+  ] ++ (builtins.attrValues outputs.nixosModules);
+
+  home-manager.extraSpecialArgs = { inherit inputs outputs; };
+
+  nixpkgs = {
+    overlays = builtins.attrValues outputs.overlays;
+    config = {
+      allowUnfree = true;
+    };
+  };
+}
---
+++ hosts/common/core/sops.nix
@@ -0,0 +1,3 @@
+{...}: {
+  # TODO: sops config stuff
+}
