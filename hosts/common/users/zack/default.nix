408c462bd36fc4c9001024efe4b066ad34661eb0
Zack Glenn <ZackGlenn05@gmail.com>
2025-04-23 21:24:14 -0600

    fix host user file
    hostname -> hostName

--- hosts/common/users/zack/default.nix
+++ hosts/common/users/zack/default.nix
@@ -4,9 +4,9 @@
     description = "Zack Glenn";
     # TODO: figure out if this is the right way to handle the ydotool group
     extraGroups = [ "networkmanager" "wheel" "ydotool" ];
+    packages = [ pkgs.home-manager ];
   };
 
-  packages = [ pkgs.home-manager ];
 
-  home-manager.users.ta = import ../../../../home/zack/${config.networking.hostname}.nix;
+  home-manager.users.zack = import ../../../../home/zack/${config.networking.hostName}.nix;
 }
