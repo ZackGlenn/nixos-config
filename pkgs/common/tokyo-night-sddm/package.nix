bc02bdff220f5916555e4f06f95c2e8a12097bf0
Zack Glenn <ZackGlenn05@gmail.com>
2025-04-27 11:28:57 -0600

    fix sdddm theme

--- pkgs/common/tokyo-night-sddm/package.nix
+++ pkgs/common/tokyo-night-sddm/package.nix
@@ -1,13 +1,6 @@
-{ qtbase
-, qtsvg
-, qtgraphicaleffects
-, qtquickcontrols2
-, wrapQtAppsHook
-, stdenvNoCC
-, fetchFromGitHub
-}:
-stdenvNoCC.mkDerivation
-rec {
+{ stdenvNoCC, fetchFromGitHub }:
+
+stdenvNoCC.mkDerivation rec {
   pname = "tokyo-night-sddm";
   version = "1..0";
   dontBuild = true;
@@ -17,16 +10,6 @@ rec {
     rev = "320c8e74ade1e94f640708eee0b9a75a395697c6";
     sha256 = "sha256-JRVVzyefqR2L3UrEK2iWyhUKfPMUNUnfRZmwdz05wL0=";
   };
-  nativeBuildInputs = [
-    wrapQtAppsHook
-  ];
-
-  propagatedUserEnvPkgs = [
-    qtbase
-    qtsvg
-    qtgraphicaleffects
-    qtquickcontrols2
-  ];
 
   installPhase = ''
     mkdir -p $out/share/sddm/themes
