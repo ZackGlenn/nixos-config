d579bc231bc621f789de86c561b3010979fa224f
Zack Glenn <ZackGlenn05@gmail.com>
2025-04-27 12:17:47 -0600

    move tokyo-night-sddm package into sddm module to avoid problem with overlays using wrong callPackage for QT packages

--- hosts/common/optional/sddm.nix
+++ hosts/common/optional/sddm.nix
@@ -1,8 +1,45 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let tokyo-night-sddm =
+  { qtbase
+  , qtsvg
+  , qtgraphicaleffects
+  , qtquickcontrols2
+  , wrapQtAppsHook
+  , stdenvNoCC
+  , fetchFromGitHub
+  }:
+  stdenvNoCC.mkDerivation
+  rec {
+    pname = "tokyo-night-sddm";
+    version = "1..0";
+    dontBuild = true;
+    src = fetchFromGitHub {
+      owner = "rototrash";
+      repo = "tokyo-night-sddm";
+      rev = "320c8e74ade1e94f640708eee0b9a75a395697c6";
+      sha256 = "sha256-JRVVzyefqR2L3UrEK2iWyhUKfPMUNUnfRZmwdz05wL0=";
+    };
+    nativeBuildInputs = [
+      wrapQtAppsHook
+    ];
+
+    propagatedUserEnvPkgs = [
+      qtbase
+      qtsvg
+      qtgraphicaleffects
+      qtquickcontrols2
+    ];
+
+    installPhase = ''
+      mkdir -p $out/share/sddm/themes
+      cp -aR $src $out/share/sddm/themes/tokyo-night
+    '';
+  };
+in {
   services.displayManager.sddm = {
     enable = true;
     theme = "tokyo-night";
   };
 
-  environment.systemPackages = [ pkgs.tokyo-night-sddm ];
+  environment.systemPackages = [ (pkgs.libsForQt5.callPackage tokyo-night-sddm {}) ];
 }
