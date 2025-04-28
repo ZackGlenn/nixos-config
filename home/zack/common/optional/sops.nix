4740eb20fb198e77095ab19a16122ead9b29a19f
Zack Glenn <ZackGlenn05@gmail.com>
2025-04-27 21:16:00 -0600

    incorporate basic sops use
    move sops.age settings to the right place
    
    move secrets attrset to the right place
    
    use zack-password not ta-password

---
+++ home/zack/common/optional/sops.nix
@@ -0,0 +1,19 @@
+{ inputs, ... }: {
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+  ];
+
+  sops = {
+    # This is the zack/dev key and needs to have been copied to this location on this host
+    age.keyFile = "/home/zack/.config/sops/age/keys.txt";
+
+    defaultSopsFile = ../../../secrets.yaml;
+    validateSopsFiles = false;
+
+    secrets = {
+      "private_keys/ta" = {
+        path = "/home/ta/.ssh/id_ed25519";
+      };
+    };
+  };
+}
--- hosts/common/users/zack/default.nix
+++ hosts/common/users/zack/default.nix
@@ -1,6 +1,11 @@
 {pkgs, inputs, config, ... }: {
+  # Decrypt zack-password to /run/secrets-for-users/ so it can be used to create the user
+  sops.secrets.zack-password.neededForUsers = true;
+  users.mutableUsers = false; # Required for password to be set via sops during system activation!
+
   users.users.zack = {
     isNormalUser = true;
+    hashedPasswordFile = config.sops.secrets.zack-password.path;
     description = "Zack Glenn";
     # TODO: figure out if this is the right way to handle the ydotool group
     extraGroups = [ "networkmanager" "wheel" "ydotool" ];
