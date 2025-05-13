046634479b4d5ba664112bea51e945d5cc3b51e6
Zack Glenn <ZackGlenn05@gmail.com>
2025-05-13 14:18:47 -0600

    feat: have nixos-anywhere generate the hardware config

--- scripts/ pi-deploy.sh
+++ scripts/ pi-deploy.sh
@@ -4,5 +4,8 @@ nix run github:nix-community/nixos-anywhere -- \
   --kexec "$(nix build --print-out-paths github:nix-community/nixos-images\#packages.aarch64-linux.kexec-installer-nixos-unstable-noninteractive)/nixos-kexec-installer-noninteractive-aarch64-linux.tar.gz" \
   --copy-host-keys \
   --extra-files ./pi-files \
+  --generate-hardware-config nixos-generate-config ./hosts/pi/hardware-configuration.nix \
   --flake 'github:ZackGlenn/nixos-config#pi' \
+  -i ~/.ssh/id_laptop \
+  -p 2211 \
   root@raspberrypi
