8b904c6d03e4107c38958203405a243499204598
Zack Glenn <ZackGlenn05@gmail.com>
2025-05-13 15:19:53 -0600

    plainly declare user ssh config

--- home/zack/common/optional/ssh.nix
+++ home/zack/common/optional/ssh.nix
@@ -16,4 +16,20 @@
   };
 
   programs.ssh.includes = [ "${config.sops.templates."extra_ssh_config".path}" ];
+
+  programs.ssh.matchBlocks = {
+    pi = {
+      user = "pi";
+      hostname = "192.168.4.11";
+      port = 2211;
+      identityFile = "~/.ssh/id_laptop";
+    };
+
+    pi-remote = {
+      user = "pi";
+      hostname = "emeraldbroam.freeddns.org";
+      port = 2211;
+      identityFile = "~/.ssh/id_laptop";
+    };
+  };
 }
