76bab7aa258fb14714412b69ca1b6b48783fba8f
Zack Glenn <ZackGlenn05@gmail.com>
2025-05-13 15:11:20 -0600

    fix: plainly declare known hosts

--- hosts/common/optional/ssh.nix
+++ hosts/common/optional/ssh.nix
@@ -1,17 +1,8 @@
-{ config, ... }: {
-  sops.secrets = {
-    "my-domain" = {
-      owner = config.users.users.zack.name;
-      inherit (config.users.users.zack) group;
+{...}: {
+  programs.ssh.knownHosts = {
+    pi = {
+      extraHostNames = [ "pi-remote" "emeraldbroam.freeddns.org" "192.168.4.11" ];
+      publicKey = "192.168.4.11 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1WbX6Zc7E4N0XrPDXDAq5KLZFXS0CRdQCzXjWEG3b0";
     };
   };
-
-  sops.templates."declared_known_hosts".content = ''
-    pi ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1WbX6Zc7E4N0XrPDXDAq5KLZFXS0CRdQCzXjWEG3b0
-    pi-remote ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1WbX6Zc7E4N0XrPDXDAq5KLZFXS0CRdQCzXjWEG3b0
-    192.168.4.11 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1WbX6Zc7E4N0XrPDXDAq5KLZFXS0CRdQCzXjWEG3b0
-    ${config.sops.secrets.my-domain} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1WbX6Zc7E4N0XrPDXDAq5KLZFXS0CRdQCzXjWEG3b0
-    '';
-
-  programs.ssh.knownHostsFiles = ["${config.sops.templates."declared_known_hosts".path}"];
 }
