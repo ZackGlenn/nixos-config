974cb3763b4ec52dde1f8cfc1467758d708c133c
Zack Glenn <ZackGlenn05@gmail.com>
2025-05-13 14:33:17 -0600

    fix: correct handing of home-level ssh secrets

--- home/zack/common/optional/ssh.nix
+++ home/zack/common/optional/ssh.nix
@@ -1,10 +1,4 @@
 { config, ... }: {
-  sops.secrets = {
-    "my-domain" = {
-      owner = config.users.users.zack.name;
-      inherit (config.users.users.zack) group;
-    };
-  };
 
   sops.templates."/home/zack/.ssh/config" = {
     content = ''
@@ -16,7 +10,7 @@
 
       Host pi-remote
         User pi
-        HostName ${config.sops.my-domain}
+        HostName ${config.sops.secrets.my-domain}
         Port 2211
         IdentityFile ~/.ssh/id_laptop
     '';
