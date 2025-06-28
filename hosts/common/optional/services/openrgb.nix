f105fcab0c0718e5ef9d0a88c3c81b52b3e84add
Zack Glenn <ZackGlenn05@gmail.com>
2025-06-28 12:42:01 -0600

    feat: have the rbg module set white at startup

--- hosts/common/optional/services/openrgb.nix
+++ hosts/common/optional/services/openrgb.nix
@@ -2,4 +2,11 @@
   services.hardware.openrgb = {
     enable = true;
   };
+
+  systemd.user.services.set_rgb = {
+    script = ''
+      openrbg --color white
+    '';
+    partOf = [ "openrbg.service" ];
+  };
 }
