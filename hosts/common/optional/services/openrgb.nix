f44c62e9997a530163144cd00d5cc9679c3fcac2
Zack Glenn <ZackGlenn05@gmail.com>
2025-07-06 14:29:16 -0600

    feat: set rgb color to match the frame color in hyprland

--- hosts/common/optional/services/openrgb.nix
+++ hosts/common/optional/services/openrgb.nix
@@ -5,7 +5,7 @@
 
   systemd.services.set_rgb = {
     script = ''
-      ${pkgs.openrgb}/bin/openrgb --mode static --color DDF9F1
+      ${pkgs.openrgb}/bin/openrgb --mode static --brightness 30 --color 7aa2f7
     '';
   wantedBy = [ "multi-user.target" ];
   after = ["openrgb.service"];
