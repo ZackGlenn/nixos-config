e324b0e4d32a4c4cb3e3e9dbc5b18aa3cff460b4
Zack Glenn <ZackGlenn05@gmail.com>
2025-07-23 14:39:20 -0600

    fix: scroll to follow cursor to fix first window not focusing with mouse

--- home/zack/common/optional/niri/input.nix
+++ home/zack/common/optional/niri/input.nix
@@ -1,28 +1,29 @@
-{...}: {
-# See https://github.com/sodiboo/niri-flake/blob/main/docs.md#programsnirisettingsinputfocus-follows-mouseenable and following
-programs.niri.settings.input = {
-  keyboard = {
+{ ... }:
+{
+  # See https://github.com/sodiboo/niri-flake/blob/main/docs.md#programsnirisettingsinputfocus-follows-mouseenable and following
+  programs.niri.settings.input = {
+    keyboard = {
       xkb = {
-          # You can set rules, model, layout, variant and options.
-          # For more information, see xkeyboard-config(7).
+        # You can set rules, model, layout, variant and options.
+        # For more information, see xkeyboard-config(7).
 
-          # For example:
-          # layout = "us,ru"
-          # options = "grp:win_space_toggle,compose:ralt,ctrl:nocaps"
+        # For example:
+        # layout = "us,ru"
+        # options = "grp:win_space_toggle,compose:ralt,ctrl:nocaps"
 
-          # If this section is empty, niri will fetch xkb settings
-          # from org.freedesktop.locale1. You can control these using
-          # localectl set-x11-keymap.
+        # If this section is empty, niri will fetch xkb settings
+        # from org.freedesktop.locale1. You can control these using
+        # localectl set-x11-keymap.
       };
 
       # Enable numlock on startup, omitting this setting disables it.
       numlock = true;
-  };
+    };
 
-  # Next sections include libinput settings.
-  # Omitting settings disables them, or leaves them at their default values.
-  # All commented-out settings here are examples, not defaults.
-  touchpad = {
+    # Next sections include libinput settings.
+    # Omitting settings disables them, or leaves them at their default values.
+    # All commented-out settings here are examples, not defaults.
+    touchpad = {
       # off = true;
       tap = true;
       # dwt = true;
@@ -34,17 +35,17 @@ programs.niri.settings.input = {
       # accel-profile = "flat"
       # scroll-method = "two-finger"
       # disabled-on-external-mouse = true;
-  };
+    };
 
-  mouse = {
+    mouse = {
       # off = true;
       # natural-scroll = true;
       # accel-speed 0.2 = true;
       # accel-profile "flat" true;
       # scroll-method "no-scroll" = true;
-  };
+    };
 
-  trackpoint = {
+    trackpoint = {
       # off = true;
       # natural-scroll = true;
       # accel-speed = 0.2
@@ -53,16 +54,16 @@ programs.niri.settings.input = {
       # scroll-button = 273
       # scroll-button-lock = true;
       # middle-emulation = true;
-  };
+    };
 
-  # Uncomment this to make the mouse warp to the center of newly focused windows.
-  # warp-mouse-to-focus = true;
+    # Uncomment this to make the mouse warp to the center of newly focused windows.
+    # warp-mouse-to-focus = true;
 
-  # Focus windows and outputs automatically when moving the mouse into them.
-  # Setting max-scroll-amount="0%" makes it work only on windows already fully on screen.
-  focus-follows-mouse = {
-    enable = true;
-    max-scroll-amount = "0%";
+    # Focus windows and outputs automatically when moving the mouse into them.
+    # Setting max-scroll-amount="0%" makes it work only on windows already fully on screen.
+    focus-follows-mouse = {
+      enable = true;
+      # max-scroll-amount = "0%";
+    };
   };
-};
 }
