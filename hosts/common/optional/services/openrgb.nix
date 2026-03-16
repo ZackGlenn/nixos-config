1f2aab8dbdbbdabca5853cced06d8e3c41a44be3
Zack Glenn <ZackGlenn05@gmail.com>
2026-03-15 23:34:30 -0600

    fix: get openrgb to cooperate

--- hosts/common/optional/services/openrgb.nix
+++ hosts/common/optional/services/openrgb.nix
@@ -1,17 +1,26 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   services.hardware.openrgb = {
     enable = true;
   };
 
-  systemd.services.set_rgb = {
-    script = ''
-      ${pkgs.openrgb}/bin/openrgb --mode static --brightness 30 --color 7aa2f7
-    '';
-  wantedBy = [ "multi-user.target" ];
-  after = ["openrgb.service"];
+  systemd.services.start_rgb = {
+    wantedBy = [
+      "multi-user.target"
+    ];
+    requires = [ "openrgb.service" ];
+    after = [ "openrgb.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 1";
+      ExecStart = "${pkgs.openrgb}/bin/openrgb --mode static --brightness 30 --color 7aa2f7";
+    };
   };
 
+  # powerManagement.resumeCommands = "${pkgs.openrgb}/bin/openrgb --mode static --brightness 30 --color 7aa2f7";
   powerManagement.resumeCommands = ''
-      ${pkgs.openrgb}/bin/openrgb --mode static --brightness 30 --color 7aa2f7
-    '';
+    systemctl restart openrgb
+    sleep 5
+    ${pkgs.openrgb}/bin/openrgb --mode static --brightness 30 --color 7aa2f7
+  '';
 }
