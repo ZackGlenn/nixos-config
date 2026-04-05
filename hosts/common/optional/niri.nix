cfbe98da93f0282416ec799113244650555b5aeb
Zack Glenn <ZackGlenn05@gmail.com>
2026-04-05 12:13:21 -0600

    feat: move to unstable niri/xwayland-satellite

--- hosts/common/optional/niri.nix
+++ hosts/common/optional/niri.nix
@@ -5,11 +5,11 @@
   nixpkgs.overlays = [ inputs.niri-flake.overlays.niri ];
   environment.systemPackages = with pkgs; [
     wl-clipboard
-    xwayland-satellite-stable # provided by niri-flake
+    xwayland-satellite-unstable # provided by niri-flake
   ];
   programs.niri = {
     enable = true;
-    package = pkgs.niri-stable; # provided by niri-flake
+    package = pkgs.niri-unstable; # provided by niri-flake
   };
   services.xserver = {
     enable = true;
