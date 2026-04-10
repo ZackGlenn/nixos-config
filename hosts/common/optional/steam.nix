5f746df08f41f928b01b3589f4c71d9b65406179
Zack Glenn <ZackGlenn05@gmail.com>
2026-04-09 22:28:40 -0600

    feat: Proton perf improvements via new ntsync kernel modules

--- hosts/common/optional/steam.nix
+++ hosts/common/optional/steam.nix
@@ -1,9 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
+    extraCompatPackages = [
+      pkgs.proton-ge-bin
+    ];
   };
 
   environment.systemPackages = [ pkgs.mangohud ];
   programs.gamemode.enable = true;
+  boot.kernelModules = [ "ntsync" ]; # Needed for wine 11.0 improvements
 }
