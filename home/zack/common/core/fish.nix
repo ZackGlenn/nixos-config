6b686bd2f8540448a55243531baa087da094920d
Zack Glenn <ZackGlenn05@gmail.com>
2026-04-09 21:15:23 -0600

    feat: shell aliases for test and dry-activate

--- home/zack/common/core/fish.nix
+++ home/zack/common/core/fish.nix
@@ -1,14 +1,15 @@
-{...}:
+{ ... }:
 {
-programs.fish = {
-  enable = true;
-  shellAliases = {
-    nrs = "sudo nixos-rebuild switch --flake ~/nixos-config/";
-    nfu = "nix flake update --flake ~/nixos-config";
-  };
-  interactiveShellInit = "
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-config/";
+      nrt = "sudo nixos-rebuild test --flake ~/nixos-config/";
+      nrd = "sudo nixos-rebuild dry-activate --flake ~/nixos-config/";
+      nfu = "nix flake update --flake ~/nixos-config";
+    };
+    interactiveShellInit = "
     set fish_greeting # Disable greeting
   ";
-};
+  };
 }
-
