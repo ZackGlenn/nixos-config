d63bf3b0a62c67ea783a7f7d605144b01c17b4e5
Zack Glenn <ZackGlenn05@gmail.com>
2026-04-26 17:37:29 -0600

    fix: use ssh-agent, new ssh key

--- hosts/common/optional/ssh.nix
+++ hosts/common/optional/ssh.nix
@@ -1,8 +1,18 @@
-{...}: {
-  programs.ssh.knownHosts = {
-    pi = {
-      extraHostNames = [ "pi-remote" "emeraldbroam.freeddns.org" "192.168.4.11" ];
-      publicKey = "192.168.4.11 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1WbX6Zc7E4N0XrPDXDAq5KLZFXS0CRdQCzXjWEG3b0";
+{ ... }:
+{
+  programs.ssh = {
+    startAgent = true;
+    knownHosts = {
+      pi = {
+        extraHostNames = [
+          "pi-remote"
+          "emeraldbroam.freeddns.org"
+          "192.168.4.11"
+        ];
+        publicKey = "192.168.4.11 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1WbX6Zc7E4N0XrPDXDAq5KLZFXS0CRdQCzXjWEG3b0";
+      };
     };
   };
+
+  services.gnome.gcr-ssh-agent.enable = false; # enabled by default for some reason
 }
