c7cb72a110570674b2eeecfc55c738e1abd7707c
Zack Glenn <ZackGlenn05@gmail.com>
2026-05-02 16:39:58 -0600

    feat: add alias for quickly editing the config

--- home/zack/common/core/fish.nix
+++ home/zack/common/core/fish.nix
@@ -7,6 +7,7 @@
       nrt = "sudo nixos-rebuild test --flake ~/nixos-config/";
       nrd = "sudo nixos-rebuild dry-activate --flake ~/nixos-config/";
       nfu = "nix flake update --flake ~/nixos-config";
+      nedit = "cd ~/nixos-config; nvim";
     };
     interactiveShellInit = "
     set fish_greeting # Disable greeting
