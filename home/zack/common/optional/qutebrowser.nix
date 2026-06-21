3d3f860d4f11902e9769e59517ba473bff9fb01e
Zack Glenn <ZackGlenn05@gmail.com>
2026-06-21 15:44:58 -0600

    fix: make qutebrowser the default browser

--- home/zack/common/optional/qutebrowser.nix
+++ home/zack/common/optional/qutebrowser.nix
@@ -90,4 +90,16 @@
       '')
     ];
   };
+
+  # Make Qutebrowser the default browser
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/about" = "org.qutebrowser.qutebrowser.desktop";
+      "x-scheme-handler/unknown" = "org.qutebrowser.qutebrowser.desktop";
+    };
+  };
 }
