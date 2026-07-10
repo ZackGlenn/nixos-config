86f213e223da6c3ebd9037191a2d02a0b98ae884
Zack Glenn <ZackGlenn05@gmail.com>
2026-07-09 19:40:18 -0600

    fix: workaround for video rendering issue in qt 6.11.1
    see https://qt-project.atlassian.net/browse/QTBUG-145344

--- home/zack/common/optional/niri/environment.nix
+++ home/zack/common/optional/niri/environment.nix
@@ -3,5 +3,8 @@
   programs.niri.settings.environment = {
     NIXOS_OZONE_WL = "1";
     QT_QPA_PLATFORM = "wayland";
+    # HACK: workaround for vidoe rendering issue on qtwebengine 6.11.1
+    # see https://qt-project.atlassian.net/browse/QTBUG-145344
+    QTWEBENGINE_FORCE_USE_GBM = "0";
   };
 }
