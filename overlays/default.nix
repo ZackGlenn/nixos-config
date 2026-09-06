1dde459f82fcadc367f706eead44fbbf9f3c10a7
Zack Glenn <ZackGlenn05@gmail.com>
2026-09-05 23:56:29 -0600

    fix: revert to older 8852CE wifi/bluetooth chip firmware to improve bluetooth stability

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,7 @@
-{inputs, ...}:
+{ inputs, ... }:
 
 let
-  additions = 
+  additions =
     final: prev:
     (prev.lib.packagesFromDirectoryRecursive {
       callPackage = prev.lib.callPackageWith final;
@@ -11,6 +11,17 @@ let
   modifications = final: prev: {
     qutebrowser = prev.qutebrowser.override {
       enableWideVine = true;
+    };
+
+    # Remove broken firmware versions to solve Realtek 8852CE wifi interfering with bluetooth
+    # Should cause the kernel to load the most recent working version, 0.27.56.14.
+    # Check with `sudo dmesg | grep rtw89` soonish after boot
+    # See: https://dredyson.com/the-hidden-truth-about-bluetooth-audio-stuttering-during-wifi-activity-on-rtl8852ce-a-complete-expert-analysis-technical-deep-dive-and-proven-workaround-guide-for-linux-users/
+    linux-firmware = prev.linux-firmware.overrideAttrs {
+      postUnpack = ''
+        rm ./source/rtw89/rtw8852c_fw-1.bin ./source/rtw89/rtw8852c_fw-2.bin
+        patchShebangs .
+      '';
     };
   };
 
@@ -32,10 +43,11 @@ let
 
 in
 {
-  default = final: prev:
+  default =
+    final: prev:
 
-  (additions final prev)
-  // (modifications final prev)
-  // (stable-packages final prev)
-  // (unstable-packages final prev);
+    (additions final prev)
+    // (modifications final prev)
+    // (stable-packages final prev)
+    // (unstable-packages final prev);
 }
